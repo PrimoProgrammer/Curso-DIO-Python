23513918babf1aa9db529909e589341312f0efee
LuanDev&Engineer <luan.sanka16@gmail.com>
2026-04-06 14:10:32 -0300

    Add files via upload

---
+++ Curso da DIO/Python/string_3.py
@@ -0,0 +1,13 @@
+print("Fatiamento de strings")
+nome = "Luan Henrique Guedes Moraes da Silva"
+
+print(nome[:36])
+print(nome[0])
+print(nome[:5])
+print(nome[5:13])
+print(nome[14:20])
+print(nome[21:27])
+print(nome[28:36])
+print(nome[5:13:2])
+print(nome[::2])
+print(nome[::-1])
