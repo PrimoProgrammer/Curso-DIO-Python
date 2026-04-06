5947f71ac26ec7433a8617b370e017a1d733d0fe
LuanDev&Engineer <luan.sanka16@gmail.com>
2026-04-06 14:08:19 -0300

    adicionado novos arquivos
    Dio fundamentos de python

---
+++ string_4.py
@@ -0,0 +1,18 @@
+nome = "Luan"
+
+mensagem = f"""
+  Olá, meu nome é {nome},
+Eu estou aprendendo Python!.
+    Essa Mensagem tem diferentes recuos."""
+
+print(mensagem)
+
+print("""
+    ================= MENU =================
+      1 - Depositar
+      2 - Sacar
+      3 - Extrato
+      0 - Sair
+         Obrigado por usar nosso sistema!
+    ========================================
+      """)
